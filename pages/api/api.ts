2b96b6a7d80b0eec22a08eefb44e74b920efdd90
Bryan Herdianto <bryan.herdianto17@gmail.com>
2025-04-13 13:27:55 +0700

    fixed type error

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -1,5 +1,3 @@
-import { headers } from "next/headers";
-
 const API_URL = "https://script.google.com/macros/s/AKfycbyn3TtsgJC4SMp5sq01Su1PdzzLFBMoc17pcThjI6YNuTrCvh9Hy6CrWqowhNUc3724/exec";
 
 export async function getPosts() {
@@ -13,7 +11,7 @@ export async function submitPost(data: { id: number; title: string; content: str
     method: "POST",
     body: JSON.stringify(data)
   }
-  const res = fetch(API_URL, options).then((res) => {
+  fetch(API_URL, options).then((res) => {
     if (!res.ok) throw new Error("Failed to submit news");
     return res.json();
   });
